292f52e8584f70b51053d223eb8a847c01c9c4c9
GHIGOA <3506454@tarentule1.ufr-info-p6.jussieu.fr>
2016-01-27 20:46:26 +0100

    first commit

---
+++ Echauffement.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+import sys
+import string
+
+alphabet = string.ascii_uppercase
+
+def calculTableMultiplication(n) :
+    #print([[i*j%n for i in range(n)] for j in range(n)])
+    listA = list()
+    listB = list()
+    for i in range(n) :
+        listA = list()
+        for j in range(n) :
+            listA.append((i*j)%n)
+        listB.append(listA)
+    return listB
+
+
+def calculInverse(m, n) :
+    listeMult = calculTableMultiplication(n)
+    for i in range(n) :
+        if listeMult[m%n][i] == 1 :
+            return i
+
+def listDiviseurs(n) :
+    return [i for i in range(1,n) if n%i == 0]
+
+def pgcd(m,n) :
+    return max(list(set(listDiviseurs(m)) & set(listDiviseurs(n))))
+
+
+def frequence(nomFichier) :
+    cptLettre = 0
+    frequence = dict([(alphabet[i], 0) for i in range(len(alphabet))])
+    file = open(nomFichier, "r")
+    texte = file.read().upper()
+    for j in range(len(texte)) :
+        if(texte[j] != '\n') :
+            cptLettre += 1
+            frequence[texte[j]] += 1
+    file.close()
+
+    for cle, valeur in frequence.items() :
+        frequence[cle] = (float(valeur) / float(cptLettre)) * 100
+    
+    return frequence
+
+    
+ 
+#print(calculTableMultiplication(12))
+
+n=12
+m=8
+
+print(calculInverse(m,n))
+print(listDiviseurs(n))
+print(pgcd(m,n))
+
+print(frequence("texte.txt"))
