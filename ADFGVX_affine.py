d973f9e57acc7699137af09476b3574e94d75a77
y0koz <yoann.ghigoff@gmail.com>
2016-03-11 10:38:12 +0100

    ADFGVX decryption

--- ADFGVX_affine.py
+++ ADFGVX_affine.py
@@ -1,14 +1,5 @@
 #! /usr/bin/python
 
-matrice = [
-    ['c','l','o','f','w','j'],
-    ['y','m','t','5','b','4'],
-    ['i','7','a','2','8','s'],
-    ['p','3','0','q','h','x'],
-    ['k','e','u','l','6','d'],
-    ['v','r','g','z','n','9']
-]
-
 def matriceTodico(mat):
     ADFGVX = "ADFGVX"
     dico = dict()
@@ -27,16 +18,19 @@ def chiffreChaine(dico, chaine):
     return chiffre
 
 def inverseClePermu(cle):
+    copyCle = cle
     for i in range(len(cle)):
-        cle[i] -= 1
+        copyCle[i] -= 1
     reversedCle = [i for i in range(len(cle))]
     for i in cle:
-        reversedCle[cle[i]] = i
+        reversedCle[copyCle[i]] = i
     return reversedCle
 
 def permuterChaine(clePermu, chaine):
     chainePermutee = ""
+    chainePermuteeHautBas = ""
     revClePermu = inverseClePermu(clePermu)
+    print revClePermu
     while((len(chaine)%len(clePermu)) != 0):
         chaine += "X"
     ind = len(chaine)/len(clePermu)
@@ -48,17 +42,46 @@ def permuterChaine(clePermu, chaine):
         for j in range(len(clePermu)):
             chainePermutee += subChaine[revClePermu[j]]
         startSub += len(clePermu)
-    return chainePermutee
     
+    for i in range(ind):
+        for j in range(ind):
+            chainePermuteeHautBas += chainePermutee[(i+j*len(clePermu))]
+
+    return chainePermuteeHautBas
+
+
+def chiffrementADFGVX(message, matrice, clePermutation):
+    return permuterChaine(clePermutation, chiffreChaine(matriceTodico(matrice), message))
+
+def dechiffrementADFGVX(message, matrice, clePermutation) :
+    nbLine = len(message)/len( clePermutation)
+    permutedTab=["" for i in range(nbLine)]
+    for j in range(len(message)) :
+        permutedTab[j%(len( clePermutation))]+=message[j]
+    len clePermutation = len( clePermutation)
+    for i in range(len clePermutation) :
+         clePermutation[i] -= 1
+    originalTab = []
+    ADFmessage = ""
+    for i in range(nbLine) :
+        originalTab.append(permut(permutedTab[i], clePermutation))
+        ADFmessage += originalTab[i]
+    message = ""
+    for i in range(len(ADFmessage)/2) :
+        message += matrice[eq_ADF_index[ADFmessage[i*2]]][eq_ADF_index[ADFmessage[i*2+1]]]
+    print message
+
+
+matrice = [
+    ['c','l','o','f','w','j'],
+    ['y','m','t','5','b','4'],
+    ['i','7','a','2','8','s'],
+    ['p','3','0','q','h','x'],
+    ['k','e','u','l','6','d'],
+    ['v','r','g','z','n','9']
+]   
 
 message = "attaque"
 clePermutation = [3,1,2,4]
 
-dico = matriceTodico(matrice)
-print dico
-chiffre = chiffreChaine(dico, message)
-print chiffre
-revCle = inverseClePermu(clePermutation)
-print revCle
-permut = permuterChaine(clePermutation, chiffre)
-print permut
+print chiffrementADFGVX(message, matrice, clePermutation)
